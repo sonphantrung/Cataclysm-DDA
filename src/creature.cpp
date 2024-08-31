601a0fb87b25deaf42db12320de1349a41f9c727
Son Phan Trung <phantrungson17@gmail.com>
2024-08-31 17:41:20 +0700

    Update creature.cpp

--- src/creature.cpp
+++ src/creature.cpp
@@ -1729,7 +1729,7 @@ void Creature::add_effect( const effect_source &source, const efftype_id &eff_id
         // Force intensity if it is duration based
         if( e.get_int_dur_factor() != 0_turns ) {
             // + 1 here so that the lowest is intensity 1, not 0
-            e.set_intensity( e.get_duration() / e.get_int_dur_factor() + 1 );
+            e.set_intensity( std::max( 1, e.get_duration() / e.get_int_dur_factor() ) );
         }
         // Bound new effect intensity by [1, max intensity]
         if( e.get_intensity() < 1 ) {
